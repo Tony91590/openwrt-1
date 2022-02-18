d5e0b95cd5404bf9f12006d63e9388508d248d66
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2022-02-19 00:05:21 +0100

    Update generic.mk

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -49,8 +49,7 @@ define Device/xiaomi_ax3600
 	PAGESIZE := 2048
 	DEVICE_DTS_CONFIG := config@ac04
 	SOC := ipq8071
-	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
-	kmod-ath10k-ct uboot-envtools
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax3600
 endef
 TARGET_DEVICES += xiaomi_ax3600
 
