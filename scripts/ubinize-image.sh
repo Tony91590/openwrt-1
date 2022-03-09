b3499e136c8d79587fad4feb37e788ea9e401fdc
Tony91590 <tony91590@hotmail.com>
2022-03-09 19:36:25 +0100

    revert

--- scripts/ubinize-image.sh
+++ scripts/ubinize-image.sh
@@ -134,7 +134,7 @@ if [ ! -r "$rootfs" -a ! -r "$kernel" -a ! "$outfile" ]; then
 	exit 1
 fi
 
-ubinize="$( command -v ubinize )"
+ubinize="$( which ubinize )"
 if [ ! -x "$ubinize" ]; then
 	echo "ubinize tool not found or not usable"
 	exit 1
