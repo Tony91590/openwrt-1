9604f146dafd5e2050ca254b491952d1dff88281
Tony91590 <tony91590@hotmail.com>
2022-03-10 19:52:22 +0100

    Boos

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -64,7 +64,6 @@ __get_band_defaults() {
 BEGIN {
         bands = ""
 }
-
 ($1 == "Band" || $1 == "") && band {
         if (channel) {
 		mode="NOHT"
@@ -78,7 +77,6 @@ BEGIN {
         }
         band=""
 }
-
 $1 == "Band" {
         band = $2
         channel = ""
@@ -86,23 +84,18 @@ $1 == "Band" {
 	ht = ""
 	he = ""
 }
-
 $0 ~ "Capabilities:" {
 	ht=1
 }
-
 $0 ~ "VHT Capabilities" {
 	vht=1
 }
-
 $0 ~ "HE Iftypes" {
 	he=1
 }
-
 $1 == "*" && $3 == "MHz" && $0 !~ /disabled/ && band && !channel {
         channel = $4
 }
-
 END {
         print bands
 }'
@@ -171,25 +164,20 @@ detect_mac80211() {
 			set wireless.radio${devidx}=wifi-device
 			set wireless.radio${devidx}.type=mac80211
 			${dev_id}
-			set wireless.radio${devidx}.channel=auto
+			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
-			set wireless.radio1.htmode=HE160
-		        set wireless.radio1.he_su_beamformee=1
-			set wireless.radio2.he_su_beamformee=1
 			set wireless.radio${devidx}.disabled=0
-			set wireless.radio0.disabled=1
 			set wireless.radio${devidx}.country=US
 			
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio0.ssid=Tenda_4BA290_AIoT
-			set wireless.default_radio2.ssid=Tenda_4BA290
-			set wireless.default_radio1.ssid=Tenda_4BA290_5G
-			set wireless.default_radio${devidx}.encryption=psk2
-			set wireless.default_radio${devidx}.key=bettyboop
+            		set wireless.default_radio0.ssid=OpenWrt_2.4G
+            		set wireless.default_radio1.ssid=OpenWrt_5G	
+			set wireless.default_radio${devidx}.encryption=sae-mixed
+                        set wireless.default_radio${devidx}.key=1234567890
 EOF
 		uci -q commit wireless
 
